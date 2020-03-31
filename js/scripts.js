289ac49498e472c5de056ad6ed6ad9bb9ea6ca48
Maria Lagunero <maria58@gmail.com>
2020-03-31 16:02:04 -0700

    add more html, css, and final spec to js scripts commented out old js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
+// var leapYear = function(year) {
+//   if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+// // jQuery user interface logic here.
+// $(document).ready(function() {
+//   $("form#leap-year").submit(function(event) {
+//     event.preventDefault();
+//     var year = parseInt($("input#year").val());
+//     var result = leapYear(year);
+//     $("#result").text(result);
+    
+    
+    
+//   });
+      
+// });
+
+// business logic
 var leapYear = function(year) {
   if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
@@ -6,19 +29,21 @@ var leapYear = function(year) {
   }
 };
 
-
-
-
-// jQuery user interface logic here.
+// user interface logic
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#result").text(result);
-    
-    
-    
+
+    $(".year").text(year);
+
+    if (!result) {                 // same as writing if (result === false)
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
   });
-      
 });
